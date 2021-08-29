99fef0e7fba0a38ffe1fdc7033aea74d9787bd3c
Dushyanth Ganesan <dushyanth.ganesan@gmail.com>
2021-08-29 00:17:54 -0400

    update player sketch with initial logic

--- public/player/player_sketch.js
+++ public/player/player_sketch.js
@@ -2,10 +2,14 @@
 // variables //
 ///////////////
 let socket;  // socket app
-let stage; // stages
+let stages = []
+stages[0] = 'name'
+stages[1] = 'waitingForHost'
+let stage; // stage variable
 
 //images
 let cardbackImgs = [];
+let cardbackOffet;
 
 // stage: enter name
 let nameInput; // name input dom
@@ -21,29 +25,30 @@ function preload() {
 	cardbackImgs[1] = loadImage('../resources/pictures/cardback-red.png');
 }
 
-////////////
-// setup //
-///////////
+//////////////////
+///// SETUP //////
+//////////////////
 function setup() {
   // canvas //
   w = windowWidth;
   h = windowHeight;
   createCanvas(w, h);
+  pixelDensity(1)  // avoids mobile slowdowns
 
   // socket
   socket = io.connect('http://localhost:3000');
 
   // name input
-  stage = "name" // set starting stage: get name
+  stage = stages[0] // set starting stage
   nameInputDom(true);
 
 	imageMode(CENTER)
 }	
 
 
-//////////
-// draw //
-//////////
+////////////////////
+////// DRAW ////////
+///////////////////
 function draw() {
   background(0);
 
@@ -58,41 +63,72 @@ function draw() {
       image(cardbackImgs[1], w-cardbackOffet, cardbackOffet);
       image(cardbackImgs[1], cardbackOffet, h-cardbackOffet);
       image(cardbackImgs[0], w-cardbackOffet, h-cardbackOffet);
-      tint(255, 127);
 
       // Fifty Six text
       textFont(titleFont);
       textAlign(CENTER)
-      textSize(170);
+      textSize(160);
       fill(color(255));
-      text('Fifty Six', w/2, h/4);
+      text('Fifty Six', w/2, h/3);
 
       // enter name text
-      textSize(30);
+      textSize(45);
       text("Enter your name:", w/2, h/2 - 60);
 
     // end stage: name
 
     case "waitingForHost":  // joined the game, now waiting for host and other players
+      // render cardbacks
+      image(cardbackImgs[0], cardbackOffet, cardbackOffet);
+      image(cardbackImgs[1], w-cardbackOffet, cardbackOffet);
+      image(cardbackImgs[1], cardbackOffet, h-cardbackOffet);
+      image(cardbackImgs[0], w-cardbackOffet, h-cardbackOffet);
+
+      // move cardbacks
+      if (cardbackOffet > -500) {
+        cardbackOffet -= 6;
+      }
+
+      // Fifty Six text
+      textFont(titleFont);
+      textAlign(CENTER)
+      textSize(160);
+      fill(color(255));
+      text('Fifty Six', w/2, h/3);
 
   }
 
 }
 
+
+////////////////////////////
+////// FUNCTIONS ///////////
+///////////////////////////
+
+// STAGE 1: NAME IMPUTTING
 // Name input and submit
 function nameInputDom(show) {
   if (show) {
-    size = w/3;
+    size = 3/8*w;
     nameInput = createInput('');
     nameInput.id('nameInput')
-    nameInput.position(w/2 - size/2 - 30, h/2);
+    nameInput.position(w/2 - size/2, h/2);
     nameInput.size(size);
+    nameInput.attribute('autocomplete', 'off')
 
     nameSubmit = createButton("submit");
     nameSubmit.id("nameSubmit");
-    nameSubmit.position(w/2 + size/2 + 10, h/2);
+    nameSubmit.position(w/2, h/2 + 120);
+    nameSubmit.mousePressed(() => {
+      if (nameInput.value() != "") {
+        console.log(nameInput.value());
+        nameInputDom(false);
+      }
+    })
 
   } else {
     nameInput.hide();
+    nameSubmit.hide();
+    stage = stages[1]
   }
 }
